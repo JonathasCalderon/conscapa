5b3c6ce72ee7b96fc8cbed0b97f0abffee30087a
JonathasCalderon <Jonathas.Calderon@fundacion-jala.org>
2020-08-21 16:56:04 -0400

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from "react"
+import { Switch, Route } from 'react-router-dom'
+import { VscMenu } from 'react-icons/vsc'
+
+import { Navbar, NavItem, DropDown, DropDownItem } from './components'
+import { Home } from './pages'
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World!</h1>
+        <div className="container">
+            <Navbar>
+                <NavItem itemContent="Conscapa" />
+                <NavItem itemContent={<VscMenu />}>
+                    <DropDown>
+                        <div className="menuBox">
+                            <DropDownItem to="/contact">
+                                Contacto
+                            </DropDownItem>
+                            <DropDownItem to="/projects">
+                                Proyectos
+                            </DropDownItem>
+                            <DropDownItem to="/about">
+                                Sobre la empresa
+                            </DropDownItem>
+                        </div>
+                    </DropDown>
+                </NavItem>
+            </Navbar>
+            <div className="content">
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
         </div>
     )
 }
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Footer from './Footer/Footer'
+import Navbar from './Navbar/Navbar'
+import NavItem from './NavItem/NavItem'
+import DropDown from './DropDown/DropDown'
+import DropDownItem from './DropDown/DropDownItem/DropDownItem'
+
+export { Footer, Navbar, NavItem, DropDown, DropDownItem }
